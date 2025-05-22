32d4e9d2e3629ddbcb563a8d26400b80c96f0ccd
Amoyt14 <ZM-HPS200@yandex.ru>
2025-05-22 15:41:03 +0300

    DONE

--- src/pages/EducationPage.vue
+++ src/pages/EducationPage.vue
@@ -10,13 +10,13 @@
         <button class="btn">Проекты</button>
       </div>
     </div>
-    <div class="megaclass" >
-      <div class="megaclass__title">УРОКИ</div>
-      <div class="card" >
-        <div class="card__body">
-          <img class="card__img" src="https://i.pinimg.com/736x/86/9f/a4/869fa4bb5d26d7ae9a9115d0bef8a536.jpg" alt=""/>
-          <div class="card__title">Название</div>
-          <div class="card__subtitle">Lorem ipsum dolor sit amet, consectetuer adipiscing elit</div>
+    <div class="megaclass" v-for="edu in educationData" :key="edu.id">
+      <div class="megaclass__title">{{edu.title}}</div>
+      <div class="card">
+        <div class="card__body" v-for="subEdu in edu.subData" :key="subEdu.id">
+          <img class="card__img" :src="subEdu.photo" alt=""/>
+          <div class="card__title">{{subEdu.subTitle}}</div>
+          <div class="card__subtitle">{{subEdu.subText}}</div>
         </div>
       </div>
     </div>
@@ -25,43 +25,127 @@
 
 <script setup lang="ts">
 
-let educationTitle = [
+let educationData = [
   {
     id: 1,
     title: "УРОКИ В ШКОЛАХ",
-    src: ["https://i.pinimg.com/736x/86/9f/a4/869fa4bb5d26d7ae9a9115d0bef8a536.jpg", "https://i.pinimg.com/736x/86/9f/a4/869fa4bb5d26d7ae9a9115d0bef8a536.jpg", "https://i.pinimg.com/736x/86/9f/a4/869fa4bb5d26d7ae9a9115d0bef8a536.jpg"],
-    subTitle: ["НАЗВАНИЕ", "НАЗВАНИЕ", "НАЗВАНИЕ"],
-    subText: ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"],
+    subData: [
+      {
+        id: 1,
+        photo: "https://i.pinimg.com/736x/86/9f/a4/869fa4bb5d26d7ae9a9115d0bef8a536.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 2,
+        photo: "https://i.pinimg.com/736x/86/9f/a4/869fa4bb5d26d7ae9a9115d0bef8a536.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 3,
+        photo: "https://i.pinimg.com/736x/86/9f/a4/869fa4bb5d26d7ae9a9115d0bef8a536.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+    ]
   },
   {
     id: 2,
     title: "ЛЕКЦИИ",
-    src: ["https://i.pinimg.com/736x/60/c0/2c/60c02c7e521aaf2335bf20fc4391f5f0.jpg", "https://i.pinimg.com/736x/60/c0/2c/60c02c7e521aaf2335bf20fc4391f5f0.jpg", "https://i.pinimg.com/736x/60/c0/2c/60c02c7e521aaf2335bf20fc4391f5f0.jpg"],
-    subTitle: ["НАЗВАНИЕ", "НАЗВАНИЕ", "НАЗВАНИЕ"],
-    subText: ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"],
-  },
+    subData: [
+      {
+        id: 1,
+        photo: "https://i.pinimg.com/736x/a5/87/57/a58757fdfdf0cdf18f38dfa48749d199.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 2,
+        photo: "https://i.pinimg.com/736x/a5/87/57/a58757fdfdf0cdf18f38dfa48749d199.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 3,
+        photo: "https://i.pinimg.com/736x/a5/87/57/a58757fdfdf0cdf18f38dfa48749d199.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      }
+      ]
+    },
   {
     id: 3,
     title: "ВЫСТАВКИ",
-    src: ["https://i.pinimg.com/736x/97/59/0f/97590ffa2b99c4e58dc5e15913063268.jpg", "https://i.pinimg.com/736x/97/59/0f/97590ffa2b99c4e58dc5e15913063268.jpg", "https://i.pinimg.com/736x/97/59/0f/97590ffa2b99c4e58dc5e15913063268.jpg"],
-    subTitle: ["НАЗВАНИЕ", "НАЗВАНИЕ", "НАЗВАНИЕ"],
-    subText: ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"],
+    subData: [
+      {
+        id: 1,
+        photo: "https://i.pinimg.com/736x/94/a4/16/94a416435a9f7cef0d947dc42b20e19b.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 2,
+        photo: "https://i.pinimg.com/736x/94/a4/16/94a416435a9f7cef0d947dc42b20e19b.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 3,
+        photo: "https://i.pinimg.com/736x/94/a4/16/94a416435a9f7cef0d947dc42b20e19b.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      }
+    ]
   },
   {
     id: 4,
     title: "ФЕСТИВАЛИ",
-    src: ["https://i.pinimg.com/736x/e7/15/68/e715686069b7d9763772ff91e2c3696b.jpg", "https://i.pinimg.com/736x/e7/15/68/e715686069b7d9763772ff91e2c3696b.jpg", "https://i.pinimg.com/736x/e7/15/68/e715686069b7d9763772ff91e2c3696b.jpg"],
-    subTitle: ["НАЗВАНИЕ", "НАЗВАНИЕ", "НАЗВАНИЕ"],
-    subText: ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"],
+    subData: [
+      {
+        id: 1,
+        photo: "https://i.pinimg.com/736x/35/97/0e/35970ebc4f444ecfccdb3590d06d6340.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 2,
+        photo: "https://i.pinimg.com/736x/35/97/0e/35970ebc4f444ecfccdb3590d06d6340.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 3,
+        photo: "https://i.pinimg.com/736x/35/97/0e/35970ebc4f444ecfccdb3590d06d6340.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      }
+    ]
   },
   {
     id: 5,
     title: "ПРОЕКТЫ",
-    src: ["https://i.pinimg.com/736x/ff/39/57/ff3957fb5a8e174bedb846296d0d863f.jpg", "https://i.pinimg.com/736x/ff/39/57/ff3957fb5a8e174bedb846296d0d863f.jpg", "https://i.pinimg.com/736x/ff/39/57/ff3957fb5a8e174bedb846296d0d863f.jpg"],
-    subTitle: ["НАЗВАНИЕ", "НАЗВАНИЕ", "НАЗВАНИЕ"],
-    subText: ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"],
+    subData: [
+      {
+        id: 1,
+        photo: "https://i.pinimg.com/736x/ff/39/57/ff3957fb5a8e174bedb846296d0d863f.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 2,
+        photo: "https://i.pinimg.com/736x/ff/39/57/ff3957fb5a8e174bedb846296d0d863f.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      },
+      {
+        id: 3,
+        photo: "https://i.pinimg.com/736x/ff/39/57/ff3957fb5a8e174bedb846296d0d863f.jpg",
+        subTitle: "название",
+        subText: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"
+      }
+    ]
   },
-
 ]
 
 </script>
@@ -82,6 +166,7 @@ let educationTitle = [
       text-align: center;
       color: black;
       padding-top: 50px;
+      margin-bottom: 10px;
     }
 
     &__btns {
@@ -102,19 +187,26 @@ let educationTitle = [
     letter-spacing: 0;
     text-align: center;
     color: #D24B35;
-    margin-top: 70px;
+    margin-top: 90px;
+    margin-bottom: 20px;
   }
 }
 
 .card {
+  width: 100%;
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+  flex-direction: row;
 
-  &__body{
-    width: 80%;
+  &__body {
+    width: 444px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 10px;
   }
+
   &__img {
     max-width: 444px;
     max-height: 296px;
@@ -129,6 +221,8 @@ let educationTitle = [
     line-height: 100%;
     letter-spacing: 0;
     color: #D24B35;
+    margin-top: 30px;
+    margin-bottom: 20px;
   }
 
   &__subtitle {
