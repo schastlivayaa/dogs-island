98c8037cc568e308a5684ac2958cb01a1d119042
Zlata Schastlivaya <s4astlivaya.zlata11@yandex.ru>
2025-05-22 23:59:10 +0300

    refactor: move scroll-button component

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="default-layout">
-      <Header/>
+      <Header></Header>
       <main><router-view/></main>
-      <Footer/>
+      <ScrollToTop />
+      <Footer></Footer>
     </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
+import ScrollToTop from '@/components/ScrollToTop.vue'
 
 export default {
   name: 'DefaultLayout',
   components: {
     Header,
-    Footer
+    Footer,
+    ScrollToTop
   }
 };
 </script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,15 +6,12 @@
       :hasMore="hasMore"
       :onLoadMore="loadMore"
     />
-    
-    <ScrollToTop />
 </template>
 
 
 <script setup>
 import Banner from '@/components/Banner.vue'
 import ArticleList from '@/components/ArticleList.vue'
-import ScrollToTop from '@/components/ScrollToTop.vue'
 
 import { onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
